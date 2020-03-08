30704285765624ee65b03e75ad299d27aafee9e6
NagaTejam <nagatejam@gmail.com>
2020-03-08 13:35:01 +0530

    Added updatePassword Service

--- user-management/src/main/java/io/github/appuhafeez/user/service/UpdateService.java
+++ user-management/src/main/java/io/github/appuhafeez/user/service/UpdateService.java
@@ -3,4 +3,5 @@ package io.github.appuhafeez.user.service;
 public interface UpdateService {
 
 	public void updateEmail(int userId, String email);
+	public void updatePassword(int userId, String oldPassword, String newPassword, String confirmPassword) throws Exception;
 }
